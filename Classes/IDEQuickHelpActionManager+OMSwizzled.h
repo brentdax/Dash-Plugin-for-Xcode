c2dce668d031f14d2fcaa563b156a6489ef77f8c
Brent Royal-Gordon <brent@architechies.com>
2013-05-04 13:00:46 -0700

    Move IDEQuickHelpActionManager overrides to their own file

---
+++ Classes/IDEQuickHelpActionManager+OMSwizzled.h
@@ -0,0 +1,16 @@
+//
+//  IDEQuickHelpActionManager+OMSwizzled.h
+//  OMQuickHelp
+//
+//  Created by Brent Royal-Gordon on 5/4/13.
+//
+//
+
+#import "ClassDumpedXcodeClasses.h"
+
+@interface NSObject (OMSwizzledIDEQuickHelpActionManager)
+
+- (void)om_openNavigableItemInDocumentationOrganizer:(IDENavigableItem*)tokenItem;
++ (void)om_performIDEQuickHelpActionManagerSwizzle;
+
+@end
